06fc18c6fc9691e3b233da2f64456b064b6aa965
thazin05 <ttthazinthazin@gmail.com>
2017-10-08 13:16:39 +0800

    update database connection

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'ftp.gear.host'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'vueAddComment'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'itfuture'),
+            'username' => env('DB_USERNAME', 'itfuture'),
+            'password' => env('DB_PASSWORD', 'ztD9zfFXSE3HE4WwBZnxeJsjuNDHvpoM5wu7J5qnR3c5yPy3KwhJqnjzmv4H'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
